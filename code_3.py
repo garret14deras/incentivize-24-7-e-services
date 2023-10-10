c44a35abee6648d2ba7f1f698ebcc3c0ae21451b
garret14deras <139816612+garret14deras@users.noreply.github.com>
2023-10-10 02:08:16 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,53 +1,13 @@
-    for item in data:
-
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-    return data
-
-
-def generate_random_data():
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-
-if __name__ == "__main__":
-import random    data = generate_random_data()
-import random
-if __name__ == "__main__":
-
-    return data
-    main()
-def generate_random_data():
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def main():
-
-    for item in data:
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-
-    for item in data:
-def main():
     data = [random.randint(1, 100) for _ in range(10)]
 def generate_random_data():
 import random
-if __name__ == "__main__":
-    main()
-
     return data
-
+    main()
+        print(f"Random Number: {item}")
     for item in data:
-def main():
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-import random
-
-    return data
 if __name__ == "__main__":
     data = generate_random_data()
+def main():
 
-        print(f"Random Number: {item}")
+
+
